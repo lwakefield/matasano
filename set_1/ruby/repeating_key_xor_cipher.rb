45169c208f1aeae79aacd7c5dc8b51ae6c016b30
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-10-05 13:48:42 +1100

    more refactoring

--- set_1/ruby/repeating_key_xor_cipher.rb
+++ set_1/ruby/repeating_key_xor_cipher.rb
@@ -2,21 +2,13 @@
 
 require_relative 'crypto'
 
-def decode_hex(hex_digest)
-    return [hex_digest.strip].pack('H*')
-end
-
-def xor_hex(hex_digest, key)
-    return sprintf('%x', hex_digest.hex ^ key.hex)
-end
-
 key = ARGV[0]
 input = ''
 while i = STDIN.gets
     input += i
 end
-
 input = input.strip
+
 repeated_key = key * (input.length.to_f / key.length).ceil
 repeated_key = repeated_key.chars.first(input.length).join
 raise "input should be the same length as the key" unless repeated_key.length == input.length
