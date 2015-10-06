754d954483e5dafba798775742d63457aaec7f8c
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-10-06 19:42:07 +1100

    solved challenge 6

--- set_1/ruby/crypto.rb
+++ set_1/ruby/crypto.rb
@@ -4,8 +4,16 @@ class Crypto
         return [hex_digest.strip].pack('H*')
     end
 
+    def self.encode_string_to_hex(plaintext)
+        return plaintext.bytes.map { |b| self.byte_to_hex_string(b) }.join
+    end
+
     def self.encode_base64(plaintext)
         return [plaintext].pack("m0")
+    end
+
+    def self.decode_base64(base64_digest)
+        return [base64_digest].unpack("m0")
     end
 
     def self.byte_to_hex_string(byte)
@@ -34,8 +42,26 @@ class Crypto
     end
 
     def self.hamming_distance(str_a, str_b)
-        xored = self.xor_raw_strings(str_a, str_b)
-        puts xored.to_s(2)
+        xored = str_a.bytes.zip(str_b.bytes).map{ |a,b| sprintf("%02b", a ^ b) }.join
+        return xored.scan(/[1]/).size
+    end
+
+    def self.break_single_byte_xor(hex_digest)
+        top_score = 0
+        top_soln = ''
+        top_key = -1
+        (0..255).each do |i|
+            key = self.byte_to_hex_string(i) * (hex_digest.length / 2)
+            xored = self.xor_hex_strings(hex_digest, key)
+            plaintext = self.decode_hex(xored)
+            score = self.score_english(plaintext)
+            if score > top_score
+                top_score = score
+                top_soln = plaintext
+                top_key = i
+            end
+        end
+        return top_key, top_soln
     end
 
 end
